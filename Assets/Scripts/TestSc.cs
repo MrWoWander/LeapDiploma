731d81c6a7b03a8fe110259dc0b5db4fc8e0e2b0
Михаил Иванов <wowivan2011@yandex.ru>
2018-05-22 21:32:36 +0300

    Test script Add first gesture

---
+++ Assets/Scripts/TestSc.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSc : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void One () {
+        print("Hello!");
+    }
+
+    public void Two ()
+    {
+        print("YES!!!");
+    }
+}
