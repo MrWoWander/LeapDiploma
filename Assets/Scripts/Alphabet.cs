c712fbc82c693de02c43bd5271a4078105943f72
Mikhail <wowivan2011@yandex.ru>
2018-05-25 13:13:20 +0300

    Create first Gesture

---
+++ Assets/Scripts/Alphabet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Alphabet : MonoBehaviour {
+
+    public Text textUI;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void A ()
+    {
+        textUI.text = "А";
+    }
+
+    public void B ()
+    {
+        textUI.text = "Б";
+    }
+    public void C ()
+    {
+        textUI.text = "В";
+    }
+
+    public void D()
+    {
+        textUI.text = "Г";
+    }
+}
