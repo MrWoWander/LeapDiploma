6bba696b3f8b09d99182d686800a2a726d57d3c3
Mikhail <wowivan2011@yandex.ru>
2018-06-13 19:37:24 +0300

    Доделал основной скрип для обучения алфавиту

--- Assets/Scripts/Gestures.cs
+++ Assets/Scripts/Gestures.cs
@@ -8,24 +8,22 @@ public struct Gesture
 {
     public GameObject gestures;
     public string name;
-    
 }
 
 public class Gestures : MonoBehaviour {
-
-
 
     public Slider slider;
     public Text letter;
     public Text textComplited;
     public Gesture[] gesturesArr;
-   
+    public float waitTimer;
 
+    bool activeTimer = false;
+    float timer;
     int gesturesValue;
     GameObject gestures;
     float testValue = 0.0f;
 
-	// Use this for initialization
 	void Start () {
         slider.value = 0;
         slider.gameObject.SetActive(false);
@@ -33,14 +31,13 @@ public class Gestures : MonoBehaviour {
         gestures = gesturesArr[gesturesValue].gestures;
         letter.text = gesturesArr[gesturesValue].name;
 
-
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+
+    void Update () {
         if (slider.gameObject.activeSelf == true)
         {
-            testValue += 1 * Time.deltaTime;
+            testValue += Time.deltaTime;
             slider.value = testValue;
         }
 
@@ -49,22 +46,39 @@ public class Gestures : MonoBehaviour {
             GesturesTrue();
         }
 
+        if (activeTimer)
+        {
+            timer += Time.deltaTime;
+            if (timer >= waitTimer)
+            {
+             
+                activeTimer = false;
+                textComplited.gameObject.SetActive(false);
+                NextGestures();
+                timer = 0;
+            }
+        }
+
     }
 
     void NextGestures()
     {
         gestures.SetActive(false);
         gesturesValue += 1;
-        gestures = gesturesArr[gesturesValue].gestures;
-        letter.text = gesturesArr[gesturesValue].name;
-        gestures.SetActive(true);
-        slider.value = 0;
+        if (gesturesValue < gesturesArr.Length)
+        {
+            gestures = gesturesArr[gesturesValue].gestures;
+            letter.text = gesturesArr[gesturesValue].name;
+            gestures.SetActive(true);
+        } else {
+            letter.text = "WIN!";
+        }
         testValue = 0;
     }
 
     public void GesturesFalse()
     {
-        if (slider.value == 1)
+        if (activeTimer)
         {
             return;
         } else {
@@ -79,42 +93,21 @@ public class Gestures : MonoBehaviour {
 
     public void GesturesTrue ()
     {
+        activeTimer = true;
+        slider.value = 0;
         slider.gameObject.SetActive(false);
         textComplited.gameObject.SetActive(true);
         textComplited.color = Color.green;
         textComplited.text = "Complited";
-        NextGestures();
+        
     }
 
-    public void A ()
+    public void ActiveGesture ()
     {
-        slider.gameObject.SetActive(true);
-    }
-
-    public void B ()
-    {
-        slider.gameObject.SetActive(true);
-    }
-    public void C ()
-    {
-        slider.gameObject.SetActive(true);
-        textComplited.gameObject.SetActive(false);
-    }
-
-    public void D()
-    {
-        slider.gameObject.SetActive(true);
-    }
-    public void I()
-    {
-        slider.gameObject.SetActive(true);
-    }
-    public void R()
-    {
-        slider.gameObject.SetActive(true);
-    }
-    public void II()
-    {
-        slider.gameObject.SetActive(true);
+        if (!activeTimer)
+        {
+            slider.gameObject.SetActive(true);
+            textComplited.gameObject.SetActive(false);
+        }
     }
 }
