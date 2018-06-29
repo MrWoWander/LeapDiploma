f04c5d767cd431b2b0817855eff3b819b07a4db7
Mikhail <wowivan2011@yandex.ru>
2018-06-29 13:13:34 +0300

    FihishHIM 2.0

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Rotate : MonoBehaviour {
+
+    public bool B;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (B)
+        {
+            transform.position = new Vector3(transform.position.x, 1.5f, 0);
+        } else
+        {
+            transform.position = new Vector3(transform.position.x, 0.9f, 0);
+        }
+
+    }
+}
